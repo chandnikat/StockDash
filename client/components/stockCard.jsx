5f224373dbe84ed8f8d519a7da608fc00728eebd
Chandni Patel <Chandni@chandni-patels-macbook.local>
2020-12-05 15:02:27 -0500

    update profitloss function

--- client/components/stockCard.jsx
+++ client/components/stockCard.jsx
@@ -4,15 +4,18 @@ import { GlobalContext } from "../context/gobalState.jsx";
 function StockCard({ stock }) {
   const { deleteStock } = useContext(GlobalContext);
 
-  const sign = stock.profitLoss < 0 ? "-" : "+";
+//~ Will need to update 100 to stock.price once price available. 
+  let profitLoss = (100 * stock.qty) - (stock.entryPrice*stock.qty)
+
+  const sign = profitLoss < 0 ? "-" : "+";
   return (
     <>
-      <li className={stock.profitLoss >= 0 ? "plus-border" : "minus-border"}>
+      <li className={profitLoss >= 0 ? "plus-border" : "minus-border"}>
         <h5>Ticker | Price | Shared | Profit/loss | Entry Price</h5>
         <br />
         {stock.ticker} <span>$100</span> <span>{stock.qty}</span>{" "}
-        <span className={stock.profitLoss >= 0 ? "plus" : "minus"}>
-          {sign}${Math.abs(stock.profitLoss)}
+        <span className={profitLoss >= 0 ? "plus" : "minus"}>
+          {sign}${Math.abs(profitLoss)}
         </span>{" "}
         <span>{stock.entryPrice}</span>{" "}
         <button onClick={() => deleteStock(stock.id)} className="delete-btn">
