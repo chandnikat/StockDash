8e587a9049cb157d8ba1225a3eed05f941a1d171
Chandni Patel <Chandni@chandni-patels-macbook.local>
2020-12-05 11:49:57 -0500

    added color to profit/loss

--- client/components/stockCard.jsx
+++ client/components/stockCard.jsx
@@ -1,15 +1,20 @@
 import React, { Component } from "react";
 
-function StockCard({stock}) {
+function StockCard({ stock }) {
   const sign = stock.profitLoss < 0 ? "-" : "+";
   return (
     <>
-
-    <li className= {stock.profitLoss >= 0 ? "plus-border" : "minus-border"}>
-    <h5>Ticker | Price | Shared | Profit/loss | Entry Price</h5><br/>
-          {stock.ticker} <span>${stock.price}</span> <span>{stock.qty}</span> <span className= {stock.profitLoss >= 0 ? "plus" : "minus"}>{sign}${Math.abs(stock.profitLoss)}</span> <span>{stock.entryPrice}</span> <button className="delete-btn">Delete</button>
-        </li>
-        </>
+      <li className={stock.profitLoss >= 0 ? "plus-border" : "minus-border"}>
+        <h5>Ticker | Price | Shared | Profit/loss | Entry Price</h5>
+        <br />
+        {stock.ticker} <span>${stock.price}</span> <span>{stock.qty}</span>{" "}
+        <span className={stock.profitLoss >= 0 ? "plus" : "minus"}>
+          {sign}${Math.abs(stock.profitLoss)}
+        </span>{" "}
+        <span>{stock.entryPrice}</span>{" "}
+        <button className="delete-btn">Delete</button>
+      </li>
+    </>
   );
 }
 
