7b91d8033780830becb577ef06c363ac338e1fba
Chandni Patel <Chandni@chandni-patels-macbook.local>
2020-12-05 14:47:58 -0500

    added add button

--- client/components/stockCard.jsx
+++ client/components/stockCard.jsx
@@ -4,13 +4,14 @@ import { GlobalContext } from "../context/gobalState.jsx";
 function StockCard({ stock }) {
   const {deleteStock} = useContext(GlobalContext)
 
+  
   const sign = stock.profitLoss < 0 ? "-" : "+";
   return (
     <>
       <li className={stock.profitLoss >= 0 ? "plus-border" : "minus-border"}>
         <h5>Ticker | Price | Shared | Profit/loss | Entry Price</h5>
         <br />
-        {stock.ticker} <span>${stock.price}</span> <span>{stock.qty}</span>{" "}
+        {stock.ticker} <span>$100</span> <span>{stock.qty}</span>{" "}
         <span className={stock.profitLoss >= 0 ? "plus" : "minus"}>
           {sign}${Math.abs(stock.profitLoss)}
         </span>{" "}
