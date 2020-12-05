4b2e4b343b91a3ad84f742c9afe525b4d47b5096
Chandni Patel <Chandni@chandni-patels-macbook.local>
2020-12-05 13:16:04 -0500

    update class names

--- client/components/balance.jsx
+++ client/components/balance.jsx
@@ -1,23 +1,26 @@
 import React, { Component } from "react";
+// export const GlobalContext = createContext(initialState);
 
 //Header COMPONENT:
 function Balance() {
+  // const {stocks} = useContext(GlobalContext)
+
   return (
     <div className="balance-container">
       <div className="total-container">
         <h4 id="balance-header">Total Profit/Loss</h4>
         <h1 id="balance">$0.00</h1>
       </div>
-      <div className="total-container">
-        <div>
-          <h4>Profit</h4>
-          <p id="profit" className="plus">
+      <div className="total-container" id="profit-container">
+        <div className="inside-profit-container">
+          <h3>Profit</h3>
+          <p className="plus">
             +$25.00
           </p>
         </div>
-        <div>
-          <h4>Loss</h4>
-          <p id="loss" className="minus">
+        <div className="inside-profit-container">
+          <h3>Loss</h3>
+          <p  className="minus">
             -$5.00
           </p>
         </div>
