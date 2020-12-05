db9d2f92c2447e45fb80876259e790395d1f1e96
Chandni Patel <Chandni@chandni-patels-macbook.local>
2020-12-05 00:01:54 -0500

    added sign variable for plus/minus

--- client/components/stockCard.jsx
+++ client/components/stockCard.jsx
@@ -1,10 +1,10 @@
 import React, { Component } from "react";
 
 function StockCard({stock}) {
+  const sign = stock.profitLoss < 0 ? "-" : "+";
   return (
     <li className="minus">
-          {stock.ticker} <span>$133.00</span> <span>{stock.qty}</span> <span>+$400</span>{" "}
-          <span>{stock.entryPrice}</span> <button className="delete-btn">Delete</button>
+          {stock.ticker} <span>$133.00</span> <span>{stock.qty}</span> <span>{sign}${stock.profitLoss}</span> <span>{stock.entryPrice}</span> <button className="delete-btn">Delete</button>
         </li>
   );
 }
--- client/context/gobalState.jsx
+++ client/context/gobalState.jsx
@@ -5,10 +5,10 @@ import AppReducer from "./appReducer.jsx";
 //Initial state:
 const initialState = {
   stocks: [
-    { id: 1, ticker: "AAPl", qty: 36, entryPrice: 115 },
-    { id: 2, ticker: "GOOGL", qty: 10, entryPrice: 1140 },
-    { id: 3, ticker: "INTC", qty: 52, entryPrice: 51 },
-    { id: 4, ticker: "CSCO", qty: 75, entryPrice: 47 },
+    { id: 1, ticker: "AAPl", price: 0, qty: 36, profitLoss: 10, entryPrice: 115 },
+    { id: 2, ticker: "GOOGL", price: 0, qty: 87, profitLoss: -10, entryPrice: 1140 },
+    { id: 3, ticker: "INTC", price: 0, qty: 14, profitLoss: 10, entryPrice: 51 },
+    { id: 4, ticker: "CSCO", price: 0, qty: 360, profitLoss: -10, entryPrice: 47 },
   ],
 };
 
