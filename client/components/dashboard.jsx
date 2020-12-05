070e90553d85f8be8dd11529c867c9692bbf3cae
Chandni Patel <Chandni@chandni-patels-macbook.local>
2020-12-04 22:50:45 -0500

    add dashbard component

--- client/components/dashboard.jsx
+++ client/components/dashboard.jsx
@@ -3,14 +3,22 @@ import React, { Component } from "react";
 function Dashboard() {
   return (
     <div id="dashboard">
-      <input className="entry" id="ticker" placeholder="Ticker"></input>
-      <input className="entry" id="shares" placeholder="# of Shares"></input>
-      <input
-        className="entry"
-        id="entryPrice"
-        placeholder="Entry Price"
-      ></input>
-      <button id="stock-button">Add</button>
+        <h3>Add a Stock</h3>
+      <form id="form">
+        <div className="form-input">
+          <label>Ticker:</label>
+          <input type="text" id="ticker" placeholder="Ticker"></input>
+        </div>
+        <div className="form-input">
+          <label>Quantity:</label>
+          <input type="number" id="shares" placeholder="# Of Shares"></input>
+        </div>
+        <div className="form-input">
+          <label>Entry Price:</label>
+          <input type="number" id="entryPrice" placeholder="Entry Price"></input>
+        </div>
+        <button className="add-btn">Add</button>
+      </form>
     </div>
   );
 }
