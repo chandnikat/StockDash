b0218eea7e59601a487d4b272342699cb3a61d20
Chandni Patel <Chandni@chandni-patels-macbook.local>
2020-12-05 17:52:17 -0500

    added working APR

--- client/components/stockCard.jsx
+++ client/components/stockCard.jsx
@@ -1,43 +1,44 @@
-import React, { Component, useState, useContext } from "react";
+import React, { Component, useState, useContext, useEffect } from "react";
 import { GlobalContext } from "../context/gobalState.jsx";
-
 
 function StockCard({ stock }) {
   const [price, setPrice] = useState(0);
   const { deleteStock } = useContext(GlobalContext);
 
-//~ Will need to update 100 to stock.price once price available. 
-  let profitLoss = (100 * stock.qty) - (stock.entryPrice*stock.qty)
+  //~ Will need to update 100 to stock.price once price available.
+  let profitLoss = 100 * stock.qty - stock.entryPrice * stock.qty;
 
   const sign = profitLoss < 0 ? "-" : "+";
 
-
   //API request:
   const token = "Q0PFL2R0GZ167DNF";
-  let symbol = stock.ticker  ;
+  let symbol = stock.ticker;
   // console.log(stock.ticker)
-  const url = "https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=" + symbol +"&apikey=" + token;
+  const url =
+    "https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=" +
+    symbol +
+    "&apikey=" +
+    token;
 
-  function getData () {
+  useEffect(() => {
     fetch(url)
-  .then(res => res.json())
-  .then(data =>{
-    console.log("INSIDE " + symbol, data["Global Quote"]["05. price"])
-    let lastPrice = data["Global Quote"]["05. price"]
-    setPrice(lastPrice)
-    
-  })
-  }
+      .then((res) => res.json())
+      .then((data) => {
+        console.log("INSIDE " + symbol, data["Global Quote"]["05. price"]);
+        let lastPrice = data["Global Quote"]["05. price"];
+        setPrice(lastPrice);
+      });
+  });
 
-  console.log("OUTSIDE " + symbol, stock.price)
-  
+  console.log("OUTSIDE " + symbol, price);
+  let currentPrice =price
 
   return (
     <>
       <li className={profitLoss >= 0 ? "plus-border" : "minus-border"}>
         <h5>Ticker | Price | Shared | Profit/loss | Entry Price</h5>
         <br />
-        {stock.ticker} <span>$100</span> <span>{stock.qty}</span>{" "}
+        {stock.ticker} <span>${Math.abs(currentPrice).toFixed(2)}</span> <span>{stock.qty}</span>{" "}
         <span className={profitLoss >= 0 ? "plus" : "minus"}>
           {sign}${Math.abs(profitLoss)}
         </span>{" "}
