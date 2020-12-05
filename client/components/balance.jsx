0e95ed508e02a3dbb8bd73e8878ac0293710ff55
Chandni Patel <Chandni@chandni-patels-macbook.local>
2020-12-05 13:39:20 -0500

    updated individual profit/loss

--- client/components/balance.jsx
+++ client/components/balance.jsx
@@ -5,24 +5,40 @@ import { GlobalContext } from "../context/gobalState.jsx";
 function Balance() {
   const { stocks } = useContext(GlobalContext);
 
+  //Total profit/Loss Calc:
   const amount = stocks.map((stock) => stock.profitLoss);
   const total = amount.reduce((acc, item) => (acc += item), 0);
   const sign = total < 0 ? "-" : "+";
 
+  //Individual Profit/Loss Calc:
+  const profit = amount
+    .filter((item) => item > 0)
+    .reduce((acc, item) => (acc += item), 0)
+    .toFixed(2);
+  const loss = (
+    amount.filter((item) => item < 0).reduce((acc, item) => (acc += item), 0) *
+    -1
+  ).toFixed(2);
+
   return (
     <div className="balance-container">
+      {/* Total Profit/Loss */}
       <div className="total-container">
         <h4 id="balance-header">Total Profit/Loss</h4>
-        <h1 id="balance" className={total >= 0 ? "plus" : "minus"}>{sign}${Math.abs(total).toFixed(2)}</h1>
+        <h1 id="balance" className={total >= 0 ? "plus" : "minus"}>
+          {sign}${Math.abs(total).toFixed(2)}
+        </h1>
       </div>
+
+      {/* Individual Profit/Loss */}
       <div className="total-container" id="profit-container">
         <div className="inside-profit-container">
           <h3>Profit</h3>
-          <p className="plus">+$25.00</p>
+          <p className="plus">+$${Math.abs(profit).toFixed(2)}</p>
         </div>
         <div className="inside-profit-container">
           <h3>Loss</h3>
-          <p className="minus">-$5.00</p>
+          <p className="minus">-$${Math.abs(loss).toFixed(2)}</p>
         </div>
       </div>
     </div>
