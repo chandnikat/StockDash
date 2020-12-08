75f3bd76f83a1f10cdbe5aa0d59e19450499e9f4
Chandni Patel <Chandni@chandni-patels-macbook.local>
2020-12-07 23:15:02 -0500

    connect mongoDB

--- server/server.js
+++ server/server.js
@@ -1,25 +1,23 @@
-const express = require('express');
-const path = require('path');
+const express = require("express");
+const path = require("path");
 const port = process.env.PORT || 3000;
 const bodyParser = require("body-parser");
-const stocks = require("/Users/Chandni/Desktop/Codesmith-Program/SoloProject/SoloProject/server/routes/stocks.js")
-const connectDB = require("/Users/Chandni/Desktop/Codesmith-Program/SoloProject/SoloProject/server/models/stockModel.js")
+const stocks = require("/Users/Chandni/Desktop/Codesmith-Program/SoloProject/SoloProject/server/routes/stocks.js");
+const connectDB = require("/Users/Chandni/Desktop/Codesmith-Program/SoloProject/SoloProject/server/models/stockModel.js");
 
-
-
-connectDB()
+connectDB();
 const app = express();
 
 app.use(express.json());
 
-if(process.env.NODE_ENV === "production"){
-  app.use('/dist', express.static(path.join(__dirname, './dist')));
+if (process.env.NODE_ENV === "production") {
+  app.use("/dist", express.static(path.join(__dirname, "./dist")));
 }
 
+app.use("/api/stocks", stocks);
 
-// app.get('/', (req, res) => {
-//   res.sendFile(path.join(__dirname, '../client/index.html'));
-// });
-app.use("/api/stocks", stocks)
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "../client/index.html"));
+});
 
-app.listen(3000, () => console.log("Server Running On Port " + port) )
+app.listen(3000, () => console.log("Server Running On Port " + port));
