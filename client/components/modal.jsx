4b853ba2a42462393ac28bc9b4d3634181d4d073
Chandni Patel <Chandni@chandni-patels-macbook.local>
2020-12-09 23:01:32 -0500

    add modal x

--- client/components/modal.jsx
+++ client/components/modal.jsx
@@ -2,12 +2,18 @@ import React, { Component, useContext, useEffect } from "react";
 import { GlobalContext } from "../context/gobalState.jsx";
 
 
-function Modal({ modalState }) {
+
+function Modal({ modalState}) {
+
+
   return (
     <div className={`modalBackground modalShowing-${modalState}`}>
       <div className="modal-Bg">
         <div className="modal-content"> 
-            <div className="modal-header"></div>
+        <div className="modal-close"><span>+</span></div>
+            <div className="modal-header">
+            Boeing
+            </div>
             <div className="modal-body"></div>
         </div>
       </div>
